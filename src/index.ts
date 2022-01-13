c658338cabf8ba7be02173b115b913dc64d7b5f0
Yuri Burk <yuriburk@gmail.com>
2022-01-13 17:40:59 -0300

    Added bot

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import { log } from "@utils/log";
+import { Whatsapp } from 'venom-bot'
 
-log('Hi')
+import { createClient } from '@utils/log'
+
+function start(client: Whatsapp) {
+  client.onMessage((message) => {
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Welcome')
+        .then((result) => {
+          console.log('Result: ', result);
+        })
+        .catch((error) => {
+          console.error('Error when sending: ', error);
+        });
+    }
+  });
+}
+
+createClient().then(client => start(client))
--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,1 +1,10 @@
-export const log = (message: string) => console.log(message)
+import { create } from 'venom-bot'
+
+export const createClient = async () => {
+  const client = await create({
+    session: 'whatsapp',
+    multidevice: false
+  })
+
+  return client
+}
